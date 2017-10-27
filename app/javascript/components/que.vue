dc3d39efd000ea0f5d88ef8e043f5ffe711dcc0a
taira120 <taira1999120@gmail.com>
2017-10-28 02:55:35 +0900

    一覧を取得できるようにした

--- app/javascript/components/que.vue
+++ app/javascript/components/que.vue
@@ -2,12 +2,21 @@
   <el-table
     :data="tableData"
     border
+    :show-header="false"
     style="width: 100%">
-    <el-table-column
-    label="Order"
-    width="180">
+    <el-table-column>
       <template slot-scope="scope">
-        <el-tag type="warning">hoge</el-tag>
+        <el-table
+          :data="scope.row.frankfurts"
+          :show-header=false
+          style="width: 100%">
+          <el-table-column>
+            <template slot-scope="scope">
+              <el-tag v-if="scope.row.ketchup" type="danger">ketchup</el-tag>
+              <el-tag v-if="scope.row.mustard" type="warning">mustard</el-tag>
+            </template>
+          </el-table-column>
+        </el-table>
       </template>
     </el-table-column>
     <el-table-column
@@ -15,11 +24,11 @@
                 width="180">
       <template slot-scope="scope">
         <el-icon name="time"></el-icon>
-        <span style="margin-left: 10px">{{ scope.row.id }}</span>
+        <span style="margin-left: 10px"> {{ scope.row.created_at }}
+        </span>
       </template>      
     </el-table-column>
-    <el-table-column
-              label="Operation">
+    <el-table-column width="100">
       <template slot-scope="scope">
         <el-button
                 size="small"
@@ -48,7 +57,6 @@ export default {
   mounted: function() {
     this.axios.get('http://localhost:5000/customers').then((response) => {
       this.tableData = response.data
-      console.log(response.data)
     })
   }
 }
