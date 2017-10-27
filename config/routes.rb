b3f19a88394b2519fb4bdc77aebca31e0391e6bf
taira120 <taira1999120@gmail.com>
2017-10-28 00:24:37 +0900

    一覧を返す

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  get '/customers', to: 'customers#show'
 
-  resources :customers, only: %i[create]
+  resources :customers, only: %i[index create]
   post '/customers/:id/done', to: 'customers#done'
 
   mount ActionCable.server => '/cable'
